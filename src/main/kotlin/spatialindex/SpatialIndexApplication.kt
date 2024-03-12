4760a6dae411e4108dc8b65065bda904c1e534e4
yeongwoo-owo <yw1221@g.skku.edu>
2024-03-12 22:10:52 +0900

    Init project

---
+++ src/main/kotlin/spatialindex/SpatialIndexApplication.kt
@@ -0,0 +1,11 @@
+package spatialindex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpatialIndexApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpatialIndexApplication>(*args)
+}
